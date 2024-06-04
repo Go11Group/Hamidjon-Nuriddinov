3bb684c9a4c1810b1115466a15779a74e0333b02
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-06-05 03:00:42 +0500

    Update homework

--- 31-dars/main.go
+++ 31-dars/main.go
@@ -4,22 +4,34 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/go-faker/faker/v4"
+	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 )
 
-const(
-	host = "localhost"
-	port = 5432
-	user = "postgres"
-	dbname = "newbase"
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	dbname   = "newbase"
 	password = "hamidjon4424"
 )
 
-func main(){
+func main() {
 	connector := fmt.Sprintf("host = %s port = %d user = %s dbname = %s password = %s sslmode = disable", host, port, user, dbname, password)
 	db, err := sql.Open("postgres", connector)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
+
+	for i := 0; i < 1_000_000; i++{
+		_, err = db.Exec("Insert Into Product values($1, $2, $3, $4)", uuid.New(), faker.Name(), faker.FirstName(), 10000)
+		if err != nil{
+			fmt.Println(err)
+		}
+		if i % 10000 == 0{
+			fmt.Println(i)
+		}
+	}
 }
--- 31-dars/main.sql
+++ 31-dars/main.sql
@@ -1,7 +1,35 @@
 create table product
 (
-    id       uuid primary key,
+    id       uuid,
     name     varchar,
     category varchar,
     cost     int
 );
+
+
+create index product_id_idx on product(id);
+
+create index product_id_idx on product(name);
+
+create index product_id_idx on product(category);
+
+create index product_id_idx on product(id, name);
+
+Explain(analyse)
+Select * from product where id = 'f2aba3c9-1800-453d-bdec-e7e2cbfee23d';
+
+Explain(analyse)
+Select * from product where name = 'Prince Rylan Kshlerin';
+
+Explain(analyse)
+Select * from product where category = 'Clare';
+
+Explain(analyse)
+Select * from product where id = 'f2aba3c9-1800-453d-bdec-e7e2cbfee23d' and name = 'Lord Ernesto Rice';
+
+
+
+-- Analiz report alohida faylda gitga yuklandi.
+
+
+
