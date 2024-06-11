d03c9473d84b02a6e8aab6dd6e32c2a45ff2f22d
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-06-11 10:04:26 +0500

    Update homework

--- 35-dars/handles/handle.go
+++ 35-dars/handles/handle.go
@@ -3,6 +3,7 @@ package handles
 import (
 	"database/sql"
 	"mymode/problem"
+	users "mymode/user"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,17 +11,22 @@ import (
 
 func NewHandler(db *sql.DB) *http.Server {
 	p := problem.NewProblemRepo(db)
+	u := users.NewUserRepo(db)
 
 	router := mux.NewRouter()
 
-	// user := router.PathPrefix("/user").Subrouter()
-	// user.HandleFunc("/create", ).Methods("POST")
-	// user.HandleFunc("/update").Methods("POST")
+	user := router.PathPrefix("/user").Subrouter()
+	user.HandleFunc("/create", u.Create).Methods("POST")
+	user.HandleFunc("/read/", u.Read).Methods("GET")
+	user.HandleFunc("/update/", u.Update).Methods("PUT")
+	user.HandleFunc("/delete/", u.Delete).Methods("Delete")
 
 	problem := router.PathPrefix("/problem/").Subrouter()
 	problem.HandleFunc("/create", p.Create).Methods("POST")
+	problem.HandleFunc("/read/", p.Read).Methods("GET")
+	problem.HandleFunc("/update/", p.Update).Methods("PUT")
+	problem.HandleFunc("/delete/", p.Delete).Methods("DELETE")
 
-	// solvedProblem := router.PathPrefix("/solvedProblem").Subrouter()
 
 	return &http.Server{Addr: ":8080", Handler: router}
 }
--- 35-dars/problem/problem.go
+++ 35-dars/problem/problem.go
@@ -6,32 +6,78 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
-type ProblemRepo struct{
+type ProblemRepo struct {
 	Db *sql.DB
 }
 
-func NewProblemRepo(db *sql.DB)*ProblemRepo{
+func NewProblemRepo(db *sql.DB) *ProblemRepo {
 	return &ProblemRepo{Db: db}
 }
 
-func (P *ProblemRepo) Create(w http.ResponseWriter, r *http.Request){
+func (P *ProblemRepo) Create(w http.ResponseWriter, r *http.Request) {
 	problem := Problem{}
 
 	body, err := io.ReadAll(r.Body)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	err = json.Unmarshal(body, &problem)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	_, err = P.Db.Exec(`Insert Into Problem(status, name, difficulty) values ($1, $2, $3)`, problem.Status, problem.Name, problem.Difficulty)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (P *ProblemRepo) Read(w http.ResponseWriter, r *http.Request){
+	id := strings.Trim(r.URL.Path, "/problem/read/")
+	problem := Problem{}
+
+	err := P.Db.QueryRow(`Select status, name, difficulty from Problem where id = $1`, id).Scan(
+		&problem.Status, &problem.Name, &problem.Difficulty)
+	if err != nil{
+		log.Fatal(err)
+	}
+	data, err := json.Marshal(problem)
+	if err != nil{
+		log.Fatal(err)
+	}
+	_, err = w.Write(data)
 	if err != nil{
 		log.Fatal(err)
 	}
 }
 
+func (P *ProblemRepo) Update(w http.ResponseWriter, r *http.Request) {
+	id := strings.Trim(r.URL.Path, "/problem/update/")
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	problem := Problem{}
+	json.Unmarshal(body, &problem)
+
+	_, err = P.Db.Exec(`Update Problem Set status = $1, name = $2, difficulty = $3 where id = $4`, 
+problem.Status, problem.Name, problem.Difficulty, id)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+}
+
+func (P *ProblemRepo) Delete(w http.ResponseWriter, r *http.Request){
+	id := strings.Trim(r.URL.Path, "/problem/update/")
+
+	_, err := P.Db.Exec(`Delete from Problem where id = $1`, id)
+	if err != nil{
+		log.Fatal(err)
+	}
+}
