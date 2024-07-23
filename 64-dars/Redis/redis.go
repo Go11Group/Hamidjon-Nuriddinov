5954e458f4335a3249ee22d477e6b80ae8cd9e69
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-07-23 13:16:08 +0500

    Update homework

--- 64-dars/Redis/redis.go
+++ 64-dars/Redis/redis.go
@@ -9,49 +9,50 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-func SaveRedis(ctx context.Context, msg model.Company){
+func SaveRedis(ctx context.Context, msg model.Company) {
 	rdb := Connect()
-	if MinValue(ctx, rdb, msg.Company) > msg.Price{
+	if MinValue(ctx, rdb, msg.Company) > msg.Price {
 		err := rdb.HSet(ctx, msg.Company, "MinPrice", msg.Price).Err()
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 		}
-	}else if MaxValue(ctx, rdb, msg.Company) < msg.Price{
+	} else if MaxValue(ctx, rdb, msg.Company) < msg.Price {
 		err := rdb.HSet(ctx, msg.Company, "MaxPrice", msg.Price).Err()
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 		}
 	}
 }
 
-func Getredis(ctx context.Context, companyName string)*model.RespCompany{
+func Getredis(ctx context.Context, companyName string) model.RespCompany {
 	rdb := Connect()
-	return &model.RespCompany{
-		Company: companyName,
+	resp := model.RespCompany{
+		Company:  companyName,
 		MaxPrice: MaxValue(ctx, rdb, companyName),
 		MinPrice: MinValue(ctx, rdb, companyName),
 	}
+	return resp
 }
 
-func MaxValue(ctx context.Context, rdb *redis.Client, companyName string)int{
+func MaxValue(ctx context.Context, rdb *redis.Client, companyName string) int {
 	price := rdb.HGet(ctx, companyName, "MaxPrice").Val()
-	if price == ""{
+	if price == "" {
 		return 0
 	}
 	p, err := strconv.Atoi(price)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	return p
 }
 
-func MinValue(ctx context.Context, rdb *redis.Client, companyName string)int{
+func MinValue(ctx context.Context, rdb *redis.Client, companyName string) int {
 	price := rdb.HGet(ctx, companyName, "MinPrice").Val()
-	if price == ""{
-		return 0
+	if price == "" {
+		return 10000000
 	}
 	p, err := strconv.Atoi(price)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	return p
--- 64-dars/api/router.go
+++ 64-dars/api/router.go
@@ -13,12 +13,12 @@ func Router() *gin.Engine {
 	router := gin.Default()
 
 	router.GET("/getPrice", func(c *gin.Context) {
-		req := model.Company{}
+		req := model.CompanyName{}
 		err := c.ShouldBindJSON(&req)
 		if err != nil {
 			log.Fatal(err)
 		}
-		resp := redisDB.Getredis(c, req.Company)
+		resp := redisDB.Getredis(c, req.Name)
 		c.JSON(http.StatusOK, resp)
 	})
 
